0c16e124540e296baee818766fd01cb1deb0a709
i403606 <i403606@git.fhict.nl>
2018-07-23 00:57:52 +0200

    Added stringSplosion exercise

---
+++ FoundationsOfProgramming/src/stringSplosion.java
@@ -0,0 +1,15 @@
+public class stringSplosion {
+
+    public static String stringSplosion(String str) {
+        String output = "";
+        for (int i = 0; i < str.length(); i++) {
+            output += str.substring(0, i+1);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+       System.out.println(stringSplosion("Test"));
+    }
+}
+
