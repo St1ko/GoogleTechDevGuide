663ed62c71cf45bb7d423c8254e08f994532c101
St1ko <stijnalbert@hotmail.com>
2018-07-23 13:02:09 +0200

    added more testcases for sumNumbers exercise

--- FoundationsOfProgramming/src/sumNumbers.java
+++ FoundationsOfProgramming/src/sumNumbers.java
@@ -23,6 +23,12 @@ public class sumNumbers {
     }
 
     public static void main(String[] args) {
+        System.out.println(sumNumbers("abc123xyz"));
         System.out.println(sumNumbers("aa11b33"));
+        System.out.println(sumNumbers("7 11"));
+        System.out.println(sumNumbers("Chocolate"));
+        System.out.println(sumNumbers("5hoco1a1e"));
+        System.out.println(sumNumbers("5$$1;;1!!"));
+
     }
 }
